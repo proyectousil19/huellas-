02df5413b8e40af82d76ed88e510a74e39534c77
Eduardocjs <51099225+Eduardocjs@users.noreply.github.com>
2019-06-03 18:40:18 -0500

    Add files via upload

---
+++ huellas/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace huellas.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
